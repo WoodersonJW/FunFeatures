593a6cb6b0ec89ccb5b1c087b74baabe5c60b92c
WoodersonJW <113423465+WoodersonJW@users.noreply.github.com>
2022-10-11 21:54:07 +0700

    Add files via upload

---
+++ FunFeatures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class FunFeatures
+    {
+        private string name = "";
+        private string email = "";
+
+        public void Start()
+        {
+            //Introduce();
+            Console.WriteLine("Let me know about yourself");
+        
+            Console.WriteLine("What is your full name? (first and last)");
+            name = Console.ReadLine();
+
+
+            Console.WriteLine("What is your email adress?");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Here is your name and email adress:");
+            Console.WriteLine(name + email);
+
+            bool done = false;
+            do
+            {
+                //Call method to read a number 1 to 7 and display the
+                //name of the day (1 = Monday, up to 7 = Sunday) with a comment
+                PredictTheDay();
+
+                //Calculate the length of a given text
+                CalculateStrengthLength
+
+                //Run again or Exit
+                done = RunAgain();
+            } while (!done);
+
+            Console.WriteLine("Welcome back, " + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment2
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Strings, selection and iteration in C#";
+            FunFeatures funObj = new FunFeatures();
+            funObj.Start();
+
+            ContinueToNextPart(); //Call the method below
+        }
+        private static void ContinueToNextPart()
+        //Static is class related and can be acessed without creating an instance/object of class
+        {
+            Console.WriteLine("\nPRESS ENTER TO CONTINUE TO THE NEXT PART");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
